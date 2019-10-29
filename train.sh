1d8a587c37015d58218ca0f2ffc810cb1de61775
Dell Du <18588220928@163.com>
2019-10-29 18:36:59 +0800

    add shortcut.

---
+++ train.sh
@@ -0,0 +1,14 @@
+DIV2K_DIR=/home/dell/ZDisk/WorkSpace/4K/dataset/DIV2K
+OUTPUT_DIR=output
+
+python train.py --dataset-dir ${DIV2K_DIR} \
+                --output-dir ${OUTPUT_DIR} \
+                --model "WDSR-B" \
+                --scale 2 \
+                --n-feats 32 \
+                --n-res-blocks 16 \
+                --expansion-ratio 6 \
+                --low-rank-ratio 0.8 \
+                --res-scale 1.0 \
+                --lr 1e-3
+
